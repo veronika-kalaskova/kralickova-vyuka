71fc7022cfc2da2841e678128e7e580656669cff
veronika-kalaskova <veronika.kalaskova@gmail.com>
2025-03-28 11:44:30 +0100

    Sign out fix

--- src/components/SidebarItems.tsx
+++ src/components/SidebarItems.tsx
@@ -29,7 +29,14 @@ export default function SidebarItems({ closeMenu }: SidebarItemsProps) {
     }
   };
 
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  let siteUrl = "";
+
+  if (process.env.NEXT_PUBLIC_SITE_URL) {
+    siteUrl = process.env.NEXT_PUBLIC_SITE_URL
+  } else {
+    siteUrl = "http://localhost:3000"
+  }
+
 
   return (
     <>
