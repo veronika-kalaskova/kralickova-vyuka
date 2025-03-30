60074389fab60b2c8a484df06fbe81987e45178d
veronika-kalaskova <veronika.kalaskova@gmail.com>
2025-03-30 17:21:38 +0200

    Celý create lektor modal

--- src/components/auth/CreateLectorModal.tsx
+++ src/components/auth/CreateLectorModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
@@ -7,7 +7,7 @@ import { Course } from "@prisma/client";
 const FormSchema = z.object({
   firstName: z.string().min(1, "Jméno je povinné"),
   lastName: z.string().min(1, "Příjmení je povinné"),
-  username: z.string().min(1, "Příjmení je povinné"),
+  username: z.string().min(1, "Uživatelské jméno je povinné"),
   password: z.string().min(1, "Heslo je povinné"),
   phone: z.string().min(1, "Telefon je povinný"),
   email: z.string().email("Neplatná e-mailová adresa"),
@@ -31,6 +31,8 @@ export default function CreateLectorModal({
     register,
     handleSubmit,
     formState: { errors },
+    watch,
+    setValue,
   } = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -40,6 +42,31 @@ export default function CreateLectorModal({
   });
 
   const [message, setMessage] = useState("");
+
+  const firstName = watch("firstName");
+  const lastName = watch("lastName");
+
+  useEffect(() => {
+    if (firstName && lastName) {
+      const suggestedUsername = `${firstName.toLowerCase()}.${lastName
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")}`;
+      setValue("username", suggestedUsername);
+    }
+  }, [firstName, lastName, setValue]);
+
+  const generatePassword = () => {
+    var length = 8,
+      charset =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setValue("password", password);
+  };
 
   const onSubmit = async (values: z.infer<typeof FormSchema>) => {
     const courseIds = values.courseIds?.map((id) => parseInt(id, 10));
@@ -66,150 +93,145 @@ export default function CreateLectorModal({
 
   return (
     <div className="fixed inset-0 z-10 flex items-center justify-center bg-[#0000007e]">
-      <div className="w-[400px] rounded-lg bg-white p-6 shadow-lg">
-        <h2 className="text-lg font-bold">Vytvořit lektora</h2>
-        <p className="text-sm text-gray-600">Role ID: {roleId}</p>
+      <div className="h-full w-full overflow-auto bg-white p-6 shadow-md sm:h-auto sm:max-w-xl md:rounded-md">
+        <h2 className="title">Vytvořit lektora</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid w-full grid-cols-2 gap-4">
+            <div className="mb-4 flex flex-col gap-2">
+              <label className="text-xs text-gray-500">Jméno*</label>
+              <input
+                type="text"
+                {...register("firstName")}
+                className="rounded-md border-[1.5px] border-gray-300 p-2 focus:border-orange-300"
+              />
+              {errors.firstName && (
+                <p className="text-xs text-red-500">
+                  {errors.firstName.message}
+                </p>
+              )}
+            </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="mt-4 space-y-4">
-          <div>
-            <label className="block text-sm font-medium">Jméno*</label>
-            <input
-              {...register("firstName")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.firstName && (
-              <p className="text-sm text-red-500">{errors.firstName.message}</p>
+            <div className="mb-4 flex flex-col gap-2">
+              <label className="text-xs text-gray-500">Příjmení*</label>
+              <input
+                {...register("lastName")}
+                className="rounded-md border-[1.5px] border-gray-300 p-2 focus:border-orange-300"
+              />
+              {errors.lastName && (
+                <p className="text-xs text-red-500">
+                  {errors.lastName.message}
+                </p>
+              )}
+            </div>
+
+            <div className="mb-4 flex flex-col gap-2">
+              <label className="text-xs text-gray-500">Email*</label>
+              <input
+                type="email"
+                {...register("email")}
+                className="rounded-md border-[1.5px] border-gray-300 p-2 focus:border-orange-300"
+              />
+              {errors.email && (
+                <p className="text-xs text-red-500">{errors.email.message}</p>
+              )}
+            </div>
+
+            <div className="mb-4 flex flex-col gap-2">
+              <label className="text-xs text-gray-500">Telefon*</label>
+              <input
+                {...register("phone")}
+                className="rounded-md border-[1.5px] border-gray-300 p-2 focus:border-orange-300"
+              />
+              {errors.phone && (
+                <p className="text-xs text-red-500">{errors.phone.message}</p>
+              )}
+            </div>
+
+            <div className="col-span-2 mb-4 flex w-full flex-col gap-2">
+              <label className="text-xs text-gray-500">Vyberte kurzy</label>
+              <p className="text-[10px] text-gray-500">
+                Jedná se pouze o kurzy, které nemají přiřazeného lektora
+              </p>
+              <select
+                multiple
+                {...register("courseIds")}
+                className="rounded-md border-[1.5px] border-gray-300 p-2 focus:border-orange-300"
+              >
+                {courses.map((course) => (
+                  <option key={course.id} value={course.id}>
+                    {course.name}
+                  </option>
+                ))}
+              </select>
+              {errors.courseIds && (
+                <p className="text-xs text-red-500">
+                  {errors.courseIds.message}
+                </p>
+              )}
+            </div>
+
+            <div className="col-span-2 mb-4 flex flex-col gap-2">
+              <label className="text-xs text-gray-500">
+                Uživatelské jméno*
+              </label>
+              <input
+                {...register("username")}
+                className="rounded-md border-[1.5px] border-gray-300 p-2 focus:border-orange-300"
+              />
+              {errors.username && (
+                <p className="text-xs text-red-500">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
+
+            <div className="col-span-2 flex flex-col gap-2">
+              <label className="text-xs text-gray-500">Heslo*</label>
+              <div className="flex gap-2">
+                <input
+                  type="text"
+                  {...register("password")}
+                  className="flex-1 rounded-md border-[1.5px] border-gray-300 p-2 focus:border-orange-300"
+                />
+                <button
+                  type="button"
+                  onClick={generatePassword}
+                  className="cursor-pointer rounded-lg bg-gray-100 px-3 py-2 font-medium text-gray-800 transition-all hover:bg-orange-100"
+                >
+                  Generovat
+                </button>
+              </div>
+              {errors.password && (
+                <p className="text-xs text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
+              <p className="text-xs text-gray-500">
+                Nezapomeňte heslo a uživatelské jméno uložit a předat uživateli.
+              </p>
+            </div>
+
+            {message && (
+              <div className="col-span-2 mt-2 text-xs text-red-500">
+                {message}
+              </div>
             )}
-          </div>
 
-          <div>
-            <label className="block text-sm font-medium">Příjmení*</label>
-            <input
-              {...register("lastName")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.lastName && (
-              <p className="text-sm text-red-500">{errors.lastName.message}</p>
-            )}
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium">username*</label>
-            <input
-              {...register("username")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.username && (
-              <p className="text-sm text-red-500">{errors.username.message}</p>
-            )}
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium">Telefon*</label>
-            <input
-              {...register("phone")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.phone && (
-              <p className="text-sm text-red-500">{errors.phone.message}</p>
-            )}
-          </div>
-
-          {/* <div>
-            <label className="block text-sm font-medium">E-mail*</label>
-            <input
-              {...register("email")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.email && (
-              <p className="text-sm text-red-500">{errors.email.message}</p>
-            )}
-          </div>
-          <div>
-            <label className="block text-sm font-medium">Priradit kurz, ktery bude lektor ucit</label>
-            <p>POZNAMKA: vybiram pouze kurz, kde neni nastaven lektor</p>
-            <input
-              {...register("kurzId")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.kurzId && (
-              <p className="text-sm text-red-500">{errors.kurzId.message}</p>
-            )}
-
-            
-          </div> */}
-
-          <div>
-            <label className="block text-sm font-medium">Vyberte kurzy</label>
-            <select
-              multiple
-              {...register("courseIds")}
-              className="w-full rounded-md border px-3 py-2"
-            >
-              {courses.map((course) => (
-                <option key={course.id} value={course.id}>
-                  {course.name}
-                </option>
-              ))}
-            </select>
-            {errors.courseIds && (
-              <p className="text-sm text-red-500">{errors.courseIds.message}</p>
-            )}
-          </div>
-
-          {/* <div>
-            <label className="block text-sm font-medium">Vyberte kurz</label>
-            <select {...register("kurzId")} className="w-full rounded-md border px-3 py-2">
-              <option value="">Vyberte kurz</option>
-              {courses.map((course) => (
-                <option key={course.id} value={course.id}>
-                  {course.name}
-                </option>
-              ))}
-            </select>
-            {errors.kurzId && <p className="text-sm text-red-500">{errors.kurzId.message}</p>}
-          </div> */}
-
-          <div>
-            <label className="block text-sm font-medium">email*</label>
-            <input
-              type="email"
-              {...register("email")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.email && (
-              <p className="text-sm text-red-500">{errors.email.message}</p>
-            )}
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium">Heslo*</label>
-            <input
-              type="password"
-              {...register("password")}
-              className="w-full rounded-md border px-3 py-2"
-            />
-            {errors.password && (
-              <p className="text-sm text-red-500">{errors.password.message}</p>
-            )}
-          </div>
-
-          {message && <div className="mt-2 text-sm">{message}</div>}
-
-          <div className="flex justify-end gap-2">
-            <button
-              type="button"
-              onClick={onClose}
-              className="rounded-md bg-gray-200 px-4 py-2 text-sm hover:bg-gray-300"
-            >
-              Zavřít
-            </button>
-            <button
-              type="submit"
-              className="rounded-md bg-blue-500 px-4 py-2 text-sm text-white hover:bg-blue-600"
-            >
-              Vytvořit
-            </button>
+            <div className="col-span-2 flex justify-end gap-2">
+              <button
+                type="button"
+                onClick={onClose}
+                className="cursor-pointer rounded-lg bg-gray-100 px-4 py-3 font-medium text-gray-800 transition-all hover:bg-orange-100"
+              >
+                Zavřít
+              </button>
+              <button
+                type="submit"
+                className="cursor-pointer rounded-lg bg-orange-400 px-4 py-3 font-medium text-white transition-all hover:bg-orange-500"
+              >
+                Vytvořit
+              </button>
+            </div>
           </div>
         </form>
       </div>
